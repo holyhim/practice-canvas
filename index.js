8a337d1b313a680311acc60b1338fc6e14047a13
holyhim <holyhim27@gmail.com>
2020-12-08 19:13:17 +0900

    init set

---
+++ index.js
@@ -0,0 +1,5 @@
+import App from "./src/App.js";
+
+window.onload = () => {
+  new App();
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+
+    document.body.appendChild(this.canvas);
+  }
+}
